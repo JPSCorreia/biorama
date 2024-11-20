7cb7506d50bec6a66bdbef236700c543e28d85f4
João Correia <jpscorreia.dev@gmail.com>
2024-11-20 08:32:22 +0000

    update modelo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,6 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
