6e3b9fb23bddda20536663ef78392a0f732b62f0
bvladi17 <b.vladi@live.com>
2024-12-10 14:36:51 +0000

    Inicio de searchbar

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SearchController;
 
 Route::get('/products', [ProductController::class, 'index']);
+Route::get('/search/suggestions', [SearchController::class, 'suggestions']);
+Route::get('/search/results', [SearchController::class, 'results']);
