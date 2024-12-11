74b3f251768bf3bc9dd9c26eecb7448d73c2d863
Lucas <lrasilvestre__@outlook.com>
2024-12-11 00:23:13 +0000

    Rergisto vendor completo - Update Codigo mais limpo

--- resources/js/Pages/RegisterVendor.jsx
+++ resources/js/Pages/RegisterVendor.jsx
@@ -15,30 +15,16 @@ import axios from "axios";
 import {router} from "@inertiajs/react";
 
 const VendorRegister = () => {
-    const [formData, setFormData] = useState({
-        nif: "",
-        phone: "",
-        address: "",
-        postal_code: "",
-        city: "",
-        date_of_birth: "",
-        iban: "",
-        is_company: false,
-        company_name: "",
-        company_nif: "",
-        company_address: "",
-        company_postal_code: "",
-        company_phone: "",
-        company_email: "",
-        vendor_photo: null
-    });
+    const [formData, setFormData] = useState({});
 
     const [error, setError] = useState("");
     const [showError, setShowError] = useState(false);
 
     // Função para criar o FormData a partir do objeto
     const createFormData = (data) => {
+
         const formData = new FormData();
+
         Object.keys(data).forEach((key) => {
             if (data[key] !== null && data[key] !== undefined) {
                 // Se for a checkbox, precisa de transformar para '1' ou '0'
@@ -62,6 +48,7 @@ const VendorRegister = () => {
 
         const data = createFormData(formData);
 
+        console.log(data);
         router.post('/registarVendedor', data, {
             withCredentials: true,
             headers: {
@@ -116,31 +103,29 @@ const VendorRegister = () => {
                     maxWidth: "470px",
                 }}
             >
-                <Paper elevation={8} sx={{ p: 2, width: "100%" }}>
+                <Paper elevation={8} sx={{p: 2, width: "100%"}}>
                     <Typography
                         variant="h5"
                         align="center"
                         gutterBottom
-                        sx={{ mb: 2 }}
+                        sx={{mb: 2}}
                     >
                         Registo de Vendedor
                     </Typography>
 
                     <form onSubmit={handleVendorRegister}>
-                        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                        <Box sx={{display: "flex", flexDirection: "column", gap: 2}}>
                             <TextField
                                 fullWidth
                                 label="NIF"
                                 name="nif"
                                 variant="outlined"
                                 required
-                                value={formData.nif}
-                                onChange={(e) =>
-                                    setFormData({
-                                        ...formData,
-                                        nif: e.target.value,
-                                    })
-                                }
+                                value={formData.nif || ""}
+                                onChange={(e) => {
+                                    const {name, value} = e.target;
+                                    setFormData((prev) => ({...prev, [name]: value}));
+                                }}
                             />
                             <TextField
                                 fullWidth
@@ -148,13 +133,11 @@ const VendorRegister = () => {
                                 name="phone"
                                 variant="outlined"
                                 required
-                                value={formData.phone}
-                                onChange={(e) =>
-                                    setFormData({
-                                        ...formData,
-                                        phone: e.target.value,
-                                    })
-                                }
+                                value={formData.phone || ""}
+                                onChange={(e) => {
+                                    const {name, value} = e.target;
+                                    setFormData((prev) => ({...prev, [name]: value}));
+                                }}
                             />
                             <TextField
                                 fullWidth
@@ -162,13 +145,11 @@ const VendorRegister = () => {
                                 name="address"
                                 variant="outlined"
                                 required
-                                value={formData.address}
-                                onChange={(e) =>
-                                    setFormData({
-                                        ...formData,
-                                        address: e.target.value,
-                                    })
-                                }
+                                value={formData.address || ""}
+                                onChange={(e) => {
+                                    const {name, value} = e.target;
+                                    setFormData((prev) => ({...prev, [name]: value}));
+                                }}
                             />
                             <TextField
                                 fullWidth
@@ -176,13 +157,11 @@ const VendorRegister = () => {
                                 name="postal_code"
                                 variant="outlined"
                                 required
-                                value={formData.postal_code}
-                                onChange={(e) =>
-                                    setFormData({
-                                        ...formData,
-                                        postal_code: e.target.value,
-                                    })
-                                }
+                                value={formData.postal_code || ""}
+                                onChange={(e) => {
+                                    const {name, value} = e.target;
+                                    setFormData((prev) => ({...prev, [name]: value}));
+                                }}
                             />
                             <TextField
                                 fullWidth
@@ -190,29 +169,25 @@ const VendorRegister = () => {
                                 name="city"
                                 variant="outlined"
                                 required
-                                value={formData.city}
-                                onChange={(e) =>
-                                    setFormData({
-                                        ...formData,
-                                        city: e.target.value,
-                                    })
-                                }
+                                value={formData.city || ""}
+                                onChange={(e) => {
+                                    const {name, value} = e.target;
+                                    setFormData((prev) => ({...prev, [name]: value}));
+                                }}
                             />
                             <TextField
                                 fullWidth
                                 label="Data de Nascimento"
                                 name="date_of_birth"
                                 type="date"
-                                InputLabelProps={{ shrink: true }}
+                                InputLabelProps={{shrink: true}}
                                 variant="outlined"
                                 required
-                                value={formData.date_of_birth}
-                                onChange={(e) =>
-                                    setFormData({
-                                        ...formData,
-                                        date_of_birth: e.target.value,
-                                    })
-                                }
+                                value={formData.date_of_birth || ""}
+                                onChange={(e) => {
+                                    const {name, value} = e.target;
+                                    setFormData((prev) => ({...prev, [name]: value}));
+                                }}
                             />
                             <TextField
                                 fullWidth
@@ -220,24 +195,20 @@ const VendorRegister = () => {
                                 name="iban"
                                 variant="outlined"
                                 required
-                                value={formData.iban}
-                                onChange={(e) =>
-                                    setFormData({
-                                        ...formData,
-                                        iban: e.target.value,
-                                    })
-                                }
+                                value={formData.iban || ""}
+                                onChange={(e) => {
+                                    const {name, value} = e.target;
+                                    setFormData((prev) => ({...prev, [name]: value}));
+                                }}
                             />
                             <FormControlLabel
                                 control={
                                     <Checkbox
-                                        checked={formData.is_company}
-                                        onChange={(e) =>
-                                            setFormData({
-                                                ...formData,
-                                                is_company: e.target.checked,
-                                            })
-                                        }
+                                        checked={formData.is_company || false}
+                                        onChange={(e) => {
+                                            const {name, checked} = e.target;
+                                            setFormData((prev) => ({...prev, [name]: checked}));
+                                        }}
                                         name="is_company"
                                     />
                                 }
@@ -251,13 +222,11 @@ const VendorRegister = () => {
                                         name="company_name"
                                         variant="outlined"
                                         required
-                                        value={formData.company_name}
-                                        onChange={(e) =>
-                                            setFormData({
-                                                ...formData,
-                                                company_name: e.target.value,
-                                            })
-                                        }
+                                        value={formData.company_name || ""}
+                                        onChange={(e) => {
+                                            const {name, value} = e.target;
+                                            setFormData((prev) => ({...prev, [name]: value}));
+                                        }}
                                     />
                                     <TextField
                                         fullWidth
@@ -265,15 +234,56 @@ const VendorRegister = () => {
                                         name="company_nif"
                                         variant="outlined"
                                         required
-                                        value={formData.company_nif}
-                                        onChange={(e) =>
-                                            setFormData({
-                                                ...formData,
-                                                company_nif: e.target.value,
-                                            })
-                                        }
+                                        value={formData.company_nif || ""}
+                                        onChange={(e) => {
+                                            const {name, value} = e.target;
+                                            setFormData((prev) => ({...prev, [name]: value}));
+                                        }}
                                     />
-                                    {/* Outros campos da empresa */}
+                                    <TextField
+                                        fullWidth
+                                        label="Morada da Empresa"
+                                        name="company_address"
+                                        variant="outlined"
+                                        value={formData.company_address || ""}
+                                        onChange={(e) => {
+                                            const {name, value} = e.target;
+                                            setFormData((prev) => ({...prev, [name]: value}));
+                                        }}
+                                    />
+                                    <TextField
+                                        fullWidth
+                                        label="Código Postal da Empresa"
+                                        name="company_postal_code"
+                                        variant="outlined"
+                                        value={formData.company_postal_code || ""}
+                                        onChange={(e) => {
+                                            const {name, value} = e.target;
+                                            setFormData((prev) => ({...prev, [name]: value}));
+                                        }}
+                                    />
+                                    <TextField
+                                        fullWidth
+                                        label="Telefone da Empresa"
+                                        name="company_phone"
+                                        variant="outlined"
+                                        value={formData.company_phone || ""}
+                                        onChange={(e) => {
+                                            const {name, value} = e.target;
+                                            setFormData((prev) => ({...prev, [name]: value}));
+                                        }}
+                                    />
+                                    <TextField
+                                        fullWidth
+                                        label="Email da Empresa"
+                                        name="company_email"
+                                        variant="outlined"
+                                        value={formData.company_email || ""}
+                                        onChange={(e) => {
+                                            const {name, value} = e.target;
+                                            setFormData((prev) => ({...prev, [name]: value}));
+                                        }}
+                                    />
                                 </>
                             )}
                             <TextField
@@ -281,13 +291,13 @@ const VendorRegister = () => {
                                 label="Fotografia do Vendedor"
                                 name="vendor_photo"
                                 type="file"
-                                InputLabelProps={{ shrink: true }}
+                                InputLabelProps={{shrink: true}}
                                 variant="outlined"
                                 onChange={(e) =>
-                                    setFormData({
-                                        ...formData,
+                                    setFormData((prev) => ({
+                                        ...prev,
                                         vendor_photo: e.target.files[0],
-                                    })
+                                    }))
                                 }
                             />
                             <Button
@@ -295,12 +305,13 @@ const VendorRegister = () => {
                                 variant="contained"
                                 color="primary"
                                 type="submit"
-                                sx={{ pt: 1 }}
+                                sx={{pt: 1}}
                             >
                                 Registar como Vendedor
                             </Button>
                         </Box>
                     </form>
+
                 </Paper>
             </Box>
         </Container>
