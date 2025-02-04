6ad72f8fd2cb635d35139c5f1fc1c5bfde806fa5
LS1lv3str3 <153948001+LS1lv3str3@users.noreply.github.com>
2025-02-04 20:55:23 +0000

    Alteração da colona coordinates da home address e store address

--- database/factories/StoreAddressFactory.php
+++ database/factories/StoreAddressFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Store;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\VendorAddress>
@@ -23,6 +24,7 @@ class StoreAddressFactory extends Factory
             'postal_code' => $this->faker->postcode(),
             'city' => $this->faker->city(),
             'comment' => $this->faker->sentence(),
+            'coordinates' => DB::raw("POINT({$this->faker->longitude(-8.9236, -8.8957)}, {$this->faker->latitude(38.5244, 38.5417)})"),
         ];
     }
 }
